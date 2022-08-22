651f1149ca3342e286721867c9303d240e91d940
niku153 <104482078+niku153@users.noreply.github.com>
2022-08-22 12:19:12 +0800

    Completed authentication page + styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Home from "./routes/home/home.component";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 export default function App() {
   const Shop = () => {
@@ -13,7 +13,7 @@ export default function App() {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
