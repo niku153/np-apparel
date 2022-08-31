0183fa5f12117a28501071df1f778c62ebb6129f
niku153 <104482078+niku153@users.noreply.github.com>
2022-08-31 13:38:13 +0800

    Added new routes for different categories

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 
-import { ReactComponent as ShoppingIcon } from "../assets/shopping-bag.svg";
+import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-import { CartContext } from "../contexts/cart.context";
+import { CartContext } from "../../contexts/cart.context";
 
 import "./cart-icon.styles.scss";
 
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,7 +1,7 @@
 import { Fragment, useContext } from "react";
 import "./navigation.styles.scss";
 
-import CartIcon from "../../cart-icon/cart-icon.component";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
